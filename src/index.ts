e7d7e28ff0b30284dd0832cc05c09ff085b33885
Tom Snelling <tomsnelling8@gmail.com>
2020-04-20 15:54:51 +0100

    replaces min.apply and max.apply

--- src/index.ts
+++ src/index.ts
@@ -3,58 +3,57 @@ import { averagePathLength, ITree } from './iTree';
 const shuffle = require('knuth-shuffle').knuthShuffle;
 
 export interface DataObject {
-    [key: string]: number,
+  [key: string]: number;
 }
 
 export class IsolationForest {
+  public trees: ITree[];
+  public X: DataObject[];
 
-	public trees: ITree[];
-	public X: DataObject[];
+  private subsamplingSize: number;
+  private numberOfTrees: number;
+  private shuffleData: boolean;
 
-	private subsamplingSize: number;
-	private numberOfTrees: number;
-	private shuffleData: boolean;
+  constructor(numberOfTrees: number = 100, subsamplingSize: number = 256) {
+    this.subsamplingSize = subsamplingSize;
+    this.numberOfTrees = numberOfTrees;
+    this.trees = [];
+    this.X = [];
+  }
 
-	constructor (numberOfTrees: number = 100, subsamplingSize: number = 256) {
-		this.subsamplingSize = subsamplingSize;
-		this.numberOfTrees = numberOfTrees;
-		this.trees = [];
-		this.X = [];
-	}
+  public fit(X: DataObject[]): ITree[] {
+    this.X = X;
+    if (this.X.length < this.subsamplingSize) {
+      this.subsamplingSize = this.X.length;
+    }
+    const heightLimit = Math.ceil(Math.log2(this.subsamplingSize));
 
-	public fit(X: DataObject[]): ITree[] {
-		this.X = X;
-		if (this.X.length < this.subsamplingSize) {
-			this.subsamplingSize = this.X.length;
-		}
-		const heightLimit = Math.ceil(Math.log2(this.subsamplingSize));
+    for (let i = 0; i < this.numberOfTrees; i++) {
+      const subsample = this.getSubsample(this.subsamplingSize);
 
-		for (let i = 0; i < this.numberOfTrees; i++) {
-			const subsample = this.getSubsample(this.subsamplingSize);
+      const iTree: ITree = new ITree(this.X, heightLimit);
+      this.trees.push(iTree);
+    }
+    return this.trees;
+  }
 
-			const iTree: ITree = new ITree(this.X, heightLimit);
-			this.trees.push(iTree);
-		} 
-		return this.trees;
-	}
+  public scores(): number[] {
+    const scoreArray: number[] = [];
+    for (const x of this.X) {
+      let pathLength: number = 0;
+      for (let j = 0; j < this.numberOfTrees; j++) {
+        pathLength += this.trees[j].pathLength(x, this.trees[j].getRootNode(), 0);
+      }
+      const meanPathLength = pathLength / this.numberOfTrees;
+      const score = Math.pow(2, -(meanPathLength / averagePathLength(this.subsamplingSize)));
+      scoreArray.push(score);
+    }
+    return scoreArray;
+  }
 
-	public scores(): number[] {
-		const scoreArray: number[] = [];
-		for (const x of this.X) {
-			let pathLength: number = 0;
-			for (let j = 0; j < this.numberOfTrees; j++) {
-				pathLength += this.trees[j].pathLength(x, this.trees[j].getRootNode(), 0);
-			}
-			const meanPathLength = pathLength / this.numberOfTrees;
-			const score = Math.pow(2, -(meanPathLength / averagePathLength(this.subsamplingSize)));
-			scoreArray.push(score);
-		}
-		return scoreArray;
-	}
-
-	private getSubsample(subsampleSize: number): DataObject[] {
-		const subsample = [];
-		const data: DataObject[] = shuffle(this.X.slice(0));
-		return data.slice(0, subsampleSize);
-	}
+  private getSubsample(subsampleSize: number): DataObject[] {
+    const subsample = [];
+    const data: DataObject[] = shuffle(this.X.slice(0));
+    return data.slice(0, subsampleSize);
+  }
 }
